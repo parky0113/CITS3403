6870da8915f117287ebda036473556e8639c43f0
Sean Park <22209962@student.uwa.edu.au>
2023-05-03 02:45:52 +0800

    first

---
+++ chat/models.py
@@ -0,0 +1,6 @@
+from chat import db
+
+class chatting(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.Text(), nullable=False)
+    create_date = db.Column(db.DateTime(), nullable=False)
