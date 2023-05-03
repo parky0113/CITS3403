d11e43af56a9898231c31a1a31ab4c886492a775
Sean Park <22209962@student.uwa.edu.au>
2023-05-03 13:27:05 +0800

    Log Out, user id for chatting data

--- chat/models.py
+++ chat/models.py
@@ -4,6 +4,8 @@ class chatting(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.Text(), nullable=False)
     create_date = db.Column(db.DateTime(), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'), nullable=False)
+    user = db.relationship('User', backref=db.backref('chatting_histroy'))
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
